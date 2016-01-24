7dae7e9d8d5bb2742542ed4ffe05e6835b6a726a
am2100 <jimnoble@xjjz.co.uk>
2016-01-24 08:17:57 +0000

    Add nginx config and php file

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Hello World!</title>
+    </head>
+    <body>
+        <img src="http://blog.osteel.me/images/2015/12/18/docker-tutorial2.gif" alt="Hello World!" />
+    </body>
+</html>
